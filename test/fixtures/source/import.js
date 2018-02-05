c4227a3fb7d75e71637abab6ff9635f08d54d75a
k88hudson <k88hudson@gmail.com>
2018-02-05 12:01:02 -0500

    Add support for ChromeUtils

--- test/fixtures/source/import.js
+++ test/fixtures/source/import.js
@@ -6,8 +6,10 @@ const {utils} = Components;
 const {foo, afw} = C.utils.import("resource://stuff", {});
 const {bar} = Cu.import("resource://stuff", {});
 const {awe} = utils.import("resource://stuff", {});
+const {wiz} = ChromeUtils.import("resource://stuff", {});
 
 const {baz} = Components.utils.flimport("resource://stuff", {});
 const {qux} = Components.u.import("resource://stuff", {});
 
 XPCOMUtils.defineLazyModuleGetter(this, "PreviewProvider", "resource:///modules/PreviewProvider.jsm");
+ChromeUtils.defineModuleGetter(this, "PreviewProvider", "resource:///modules/PreviewProvider.jsm");
